dc0db47e749ddab52429dc7bca7eeade7d9aa8db
Diana <diana_nunez10@yahoo.com>
2021-06-09 17:31:28 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+
+
+acdc = Artist.create(name: "AC/DC", bio: "Let's Rock!")
+shakira = Artist.create(name: "Shakira", bio: "I like to dance!")
+
+rock = Genre.create(name: "rock")
+pop = Genre.create(name: "pop")
+
+bb = Song.create(name: "Back in Black", artist_id: acdc.id, genre_id: rock.id)
+hdl = Song.create(name: "Hips Don't Lie", artist_id: shakira.id, genre_id: pop.id)
